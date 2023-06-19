5836d9dbd715499d67266deff48d4004e1b75e02
Gabriel S. A <falconrei123@gmail.com>
2023-06-19 20:46:53 -0300

    header feito e footer so o tamanho

--- sublimoon-front-end/src/App.vue
+++ sublimoon-front-end/src/App.vue
@@ -1,12 +1,38 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <!--  <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+    <a href="../src/views/PgInicialView.vue">
+    <img id="logo" src="../src/assets/iconeSublimoon.png" alt="Icone Sublimoon"> 
+    </a>
+    <div class="divPlaceholder">
+      <input type="text" name="Buscar" id="BuscarInput" placeholder="Buscar">
+      <button id="botaoSearch">🔍</button>
+    </div>
+    <router-link to="" class="categ">Masculino</router-link>
+    <router-link to="" class="categ">Feminino</router-link>
+    <router-link to="" class="categ">Canecas</router-link>
+    <router-link to="" class="categ">Garrafas</router-link>
+    <router-link to="" class="categ">Personalizados</router-link>
+    <router-link to="" class="icons">🛒</router-link>
+    <router-link to="" class="icons">👤</router-link>
+    
   </nav>
+  <footer>
+  </footer>
   <router-view/>
 </template>
 
 <style lang="scss">
+
+#logo{
+  width: 5vw;
+  height: auto;
+}
+#logo:hover{
+  transition: 1s;
+  scale: 120%;
+}
 
 $primary: #900;
 $enable-shadows: true;
@@ -23,15 +49,68 @@ $prefix: "mo-";
 }
 
 nav {
-  padding: 30px;
+  //padding: 30px;
+  display: flex;
+  height: 13vh;
+  margin: auto;
+  justify-content: space-around;
+  align-items: center;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #5e17eb;
+      font-size: 2vw;
+      font-weight: 600;
+      text-decoration: none;
+      font-family: system-ui, 'Segoe UI', Roboto, Oxygen, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
-  }
+  
+}
+
+.categ{
+  color: #153b75;
+      font-size: 2vw;
+      font-weight: 600;
+      text-decoration: none;
+      font-family: system-ui, 'Segoe UI', Roboto, Oxygen, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.categ:hover{
+  transition: 0.5s;
+  color: #2669c7;
+}
+.icons{
+  font-size: 2vw;
+  text-decoration: none;
+}
+.icons:hover{
+  transition: 0.5s;
+  scale: 150%;
+}
+
+.divPlaceholder{
+  background-color: #3463a3;
+  padding: 0.7% 1.4%;
+  border-radius: 8px;
+}
+
+#BuscarInput{
+  border: none;
+}
+#BuscarInput:hover{
+  transition: 0.5s;
+  background-color: rgb(192, 192, 192);
+  color: black;
+}
+#botaoSearch{
+  border: none;
+  background-color:white ;
+}
+#botaoSearch:hover{
+  transition: 0.5s;
+  background-color: rgb(192, 192, 192);
+}
+footer{
+  background-color: rgb(57, 57, 57);
+  height: 20vh;
 }
 </style>
