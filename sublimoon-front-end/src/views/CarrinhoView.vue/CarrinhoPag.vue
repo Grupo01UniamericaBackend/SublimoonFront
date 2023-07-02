c276a2449fc28493de0c9cb0baaa89da05c2bdae
Gabriel S. A <falconrei123@gmail.com>
2023-07-01 23:24:06 -0300

    titulo e botao de finalizar no cadastro

--- sublimoon-front-end/src/views/CarrinhoView.vue/CarrinhoPag.vue
+++ sublimoon-front-end/src/views/CarrinhoView.vue/CarrinhoPag.vue
@@ -1,5 +1,11 @@
 <template>
+    <div class="container-carrinho">
 
+        <h1>Itens no Carrinho:</h1>
+
+        <router-link type="button" class="btn btn-success" to="">Finalizar Pedido</router-link>
+
+    </div>
 
 </template>
 
@@ -10,5 +16,11 @@
 
 <style lang="scss">
 
+.container-carrinho{
+    min-height: 70vh;
+}
+h1{
+    font-weight: 700;
+}
 
 </style>
