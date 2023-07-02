f4d07964c16a8d7f754729c2b6b55ac04f986451
Gabriel S. A <falconrei123@gmail.com>
2023-07-01 21:02:18 -0300

    dropdown perfil ai tomaaa

--- sublimoon-front-end/src/components/NavBar.vue
+++ sublimoon-front-end/src/components/NavBar.vue
@@ -1,40 +1,45 @@
 <template>
-    <header>
-        <nav>
-            <a href="/">
-            <img id="logo" src="../assets/iconeSublimoon.png" alt="Icone Sublimoon"> 
-            </a>
-            <div class="divPlaceholder">
-           <!-- <input type="text" name="Buscar" id="BuscarInput" placeholder="Buscar">
+  <header>
+    <nav>
+      <a href="/">
+        <img id="logo" src="../assets/iconeSublimoon.png" alt="Icone Sublimoon">
+      </a>
+      <div class="divPlaceholder">
+        <!-- <input type="text" name="Buscar" id="BuscarInput" placeholder="Buscar">
             <button id="botaoSearch">🔍</button> -->
-            <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-            </div>
-            <router-link to="" class="categ">Canecas</router-link>
-            <router-link to="" class="categ">Camiseta</router-link>
-            <router-link to="" class="categ">Garrafinhas</router-link>
-            <router-link to="" class="categ">Copos-Térmicos</router-link>
-            <router-link to="/Carrinho" class="icons">🛒</router-link>
-            <router-link to="/Favoritos" class="icons">💙</router-link>
-            <router-link to="/Usuario" class="icons">👤</router-link>
-         </nav>
-         <div class="barra"></div>
-         <router-view/>
-    </header>
+        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+      </div>
+      <router-link to="" class="categ">Canecas</router-link>
+      <router-link to="" class="categ">Camiseta</router-link>
+      <router-link to="" class="categ">Garrafinhas</router-link>
+      <router-link to="" class="categ">Copos-Térmicos</router-link>
+      <router-link to="/Carrinho" class="icons">🛒</router-link>
+      <router-link to="/Favoritos" class="icons">💙</router-link>
+          <p data-bs-toggle="dropdown" id="iconP">👤</p>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/usuario">Usuário</a></li>
+           <li><a class="dropdown-item" href="/Estoque">Estoque</a></li>
+          <li><a class="dropdown-item" href="#">Sair</a></li>
+          </ul>
+    </nav>
+    <div class="barra"></div>
+    <router-view />
+  </header>
 </template>
 
 <script lang="ts">
-  export default {
-    name: "NavBar",
-  };
-  </script>
+export default {
+  name: "NavBar",
+};
+</script>
 
 <style lang="scss">
-
-#logo{
+#logo {
   width: 5vw;
   height: auto;
 }
-#logo:hover{
+
+#logo:hover {
   transition: 1s;
   scale: 120%;
 }
@@ -57,43 +62,54 @@ nav {
   align-items: center;
 
 
-    &.router-link-exact-active {
-      color: #5e17eb;
-      font-size: 2vw;
-      font-weight: 600;
-      text-decoration: none;
-      font-family: system-ui, 'Segoe UI', Roboto, Oxygen, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-  
+  &.router-link-exact-active {
+    color: #5e17eb;
+    font-size: 2vw;
+    font-weight: 600;
+    text-decoration: none;
+    font-family: system-ui, 'Segoe UI', Roboto, Oxygen, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
 }
 
-.categ{
+.categ {
   color: #153b75;
-      font-size: 1.7vw;
-      font-weight: 600;
-      text-decoration: none;
-      font-family: system-ui, 'Segoe UI', Roboto, Oxygen, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 1.7vw;
+  font-weight: 600;
+  text-decoration: none;
+  font-family: system-ui, 'Segoe UI', Roboto, Oxygen, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
-.categ:hover{
+
+.categ:hover {
   transition: 0.5s;
   color: #2669c7;
 }
-.icons{
+
+.icons {
   font-size: 2vw;
   text-decoration: none;
 }
-.icons:hover{
+
+.icons:hover {
+  transition: 0.5s;
+  scale: 150%;
+}
+#iconP{
+  font-size: 2vw;
+  text-decoration: none;
+}
+#iconP:hover{
   transition: 0.5s;
   scale: 150%;
 }
 
-.divPlaceholder{
+.divPlaceholder {
   background-color: #3463a3;
   padding: 0.4% 0.4%;
   border-radius: 8px;
 }
-.barra{
+
+.barra {
   background-color: #3463a3;
   height: 25px;
-}
-</style>
+}</style>
